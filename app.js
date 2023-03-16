0272c4164bc8e59f8d93b1a156df18ce60afa028
KausaChangs <changayakausa@gmail.com>
2023-03-16 15:21:11 +0200

    initialised all variables in app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const squares = document.querySelectorAll(".square");
+const mole = doocument.querySelector(".mole");
+
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
